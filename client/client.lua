f89bc5ae1571e46b6d56ed001329cde3f78c0df5
DK-DonnieKongy <52999643+DK-DonnieKongy@users.noreply.github.com>
2019-07-19 11:16:10 +1000

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -3,63 +3,58 @@
 --		By DK - 2019...	Dont forget your Bananas!			--
 ------------------------------------------------------------------------------
 
---[[ Loading ESX Object Dependancies ]]--
+--[ Loading ESX Object Dependancies ]--
 
 ESX = nil
 
 Citizen.CreateThread(function()
-		if ESX == nil do
+		while ESX == nil do
 			TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+			Citizen.Wait(250)
 		end
 end)
 
---[[ ESX Loaded - Generate Code Below ]]--
+--[ ESX Loaded - Generate Code Below ]--
 
 ------------------------------------------------------------------------------
 
 -- The math is, (( Config.PopulationNumber - (Config.Players * 2.25 )) / Config.1000)  =  0.3880 or less
 -- The deal is to reduce the overal feel of the city as the player population is introduced.
 		
--- The natives take a [0 as nothing, 1 as normal, 2 as doubled etc] value. 
+-- The natives take a .0 as nothing, 1 as normal, 2 as doubled etc value. 
 -- So why not make it like 0.10 @ 128 Players??
 
 ------------------------------------------------------------------------------
-
+--	ThreadFUNctions															--
 ------------------------------------------------------------------------------
 
 Citizen.CreateThread(function()
-	local PlayerPed = GetPlayerPed(-1)
-	local iPlayer 	= GetEntityCoords(PlayerPed, true)
+	local iPlayer = GetEntityCoords(PlayerPedId())
 	
-	while true do		-- Every Frame!
-		SetVehicleDensityMultiplierThisFrame((Config['TrafficX'] - Config['iPlayers']) / Config['Divider'])
-		SetPedDensityMultiplierThisFrame((Config['PedestrianX'] - Config['iPlayers']) / Config['Divider'])
-		SetRandomVehicleDensityMultiplierThisFrame((Config['TrafficX'] - Config['iPlayers']) / Config['Divider'])
-		SetParkedVehicleDensityMultiplierThisFrame((Config['ParkedX'] - Config['iPlayers']) / Config['Divider'])
-		SetScenarioPedDensityMultiplierThisFrame((Config['PedestrianX'] - Config['iPlayers']) / Config['Divider']), ((Config['PedestrianX'] - Config['iPlayers']) / Config.[Divider])				
-		ClearAreaOfCops(iPlayer['x'] iPlayer['y'] iPlayer['z'], 5000.0)
-		RemoveVehiclesFromGeneratorsInArea(iPlayer['x'] - 35.0, iPlayer['y'] - 35.0, iPlayer['z'] - 35.0, iPlayer['x'] + 35.0, iPlayer['y'] + 35.0, iPlayer['z'] + 35.0);
+	while Config.Switch == true do
+		Citizen.Wait(0)							-- Every Frame!
+		for i = 0, 15 do						-- For all gangs and emergancy services.	
+			EnableDispatchService(i, Config.Dispatch)		-- Disable responding/dispatch.
+		end			
+		SetVehicleDensityMultiplierThisFrame((Config.TrafficX - Config.iPlayers) / Config.Divider)
+		SetPedDensityMultiplierThisFrame((Config.PedestrianX - Config.iPlayers) / Config.Divider)
+		SetRandomVehicleDensityMultiplierThisFrame((Config.TrafficX - Config.iPlayers) / Config.Divider)
+		SetParkedVehicleDensityMultiplierThisFrame((Config.ParkedX - Config.iPlayers) / Config.Divider)
+		SetScenarioPedDensityMultiplierThisFrame((Config.PedestrianX - Config.iPlayers) / Config.Divider, (Config.PedestrianX - Config.iPlayers) / Config.Divider)
+		ClearAreaOfCops(iPlayer.x, iPlayer.y, iPlayer.z, 5000.0)
+		RemoveVehiclesFromGeneratorsInArea(iPlayer.x - 45.0, iPlayer.y - 45.0, iPlayer.z - 15.0, iPlayer.x + 45.0, iPlayer.y + 45.0, iPlayer.z + 15.0);
 		SetGarbageTrucks(0)
 		SetRandomBoats(0)
-		Citizen.Wait(0)	-- Every Frame!
-	end
-end)
-
-Citizen.CreateThread(function()
-	while true do		-- Every Frame!
-		for i = 1, 15 do													-- For all gangs and emergancy services.	
-			EnableDispatchService(i, Config['Dispatch'])	-- Disable responding/dispatch.
-		end	
-		Citizen.Wait(0)	-- Every Frame!	
 	end
 end)
 
 ------------------------------------------------------------------------------
-
+--	FUNctions																--
 ------------------------------------------------------------------------------
 
 function Check()			-- Tell that Global Variable to be beautiful.
 
+	Config.Switch = false
 	local Multiplier = 0		-- Player Count.
 
 	for i in ipairs(GetActivePlayers()) do
@@ -67,33 +62,22 @@ function Check()			-- Tell that Global Variable to be beautiful.
 
 		if DoesEntityExist(iPed) then
 			Multiplier = Multiplier + 1
-		else
-		print('	^0[^1Alert^0] : | Traffic has not been altered. - Nill players. | : [^1Alert^0] ')	
 		end
 	end
 		
 	if Multiplier ~= 0 then
-		Config[iPlayers] = (Config[Static] * Multiplier)
-		print('	^0[^1Alert^0] : | Traffic Adjusted by [' .. Multiplier .. '] players, [-' .. Config[iPlayers] ..  '] total. | : [^1Alert^0] ')		
-	elseif Multiplier == 0 then
-		print('	^0[^1Alert^0] : | Traffic has not been altered as there are no players. ' .. Config[iPlayers] .. '| : [^1Alert^0] ')	
+		Config.iPlayers = Config.Static * Multiplier
 	end	
-	
+
+	Wait(100)
+	Config.Switch = true
 end
 	
 ------------------------------------------------------------------------------
-
+--	ReigsterClientEvents													--
 ------------------------------------------------------------------------------
 
 RegisterNetEvent('LosOce_TA:Force')
-AddEventHandler('LosOce_TA:Force', function(xPlayers)
-	local xPlayers  = ESX.GetPlayers()
-	local iPlayer   = PlayerID()
-	
-	for _, v in ipairs(xPlayers) do
-		if xPlayers.v == iPlayer then
-			Check()
-		end return
-	end
-
+AddEventHandler('LosOce_TA:Force', function()
+	Check()
 end)
